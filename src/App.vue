052b7a8e37fb55e264b1d4be9cda6db4df7d1155
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-03-25 15:34:04 +0100

    [add] inizio esercizio

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import Main from '.components/Main.vue'
+    export default {
+        components:{
+          Main
+        }
+    }
+</script>
+
+<template>
+   <Main/>
+
+</template>
+
+<style lang="scss" scoped>
+   @use './assets/scss/main.scss';  
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,14 @@
+<script>
+    export default {
+        
+    }
+</script>
+
+<template>
+   <div>Ciao</div>
+
+</template>
+
+<style lang="scss" scoped>
+   
+</style>
