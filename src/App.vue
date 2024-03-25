3df3b1898cdbb626fdecc166651c4ecd9324c0d9
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-03-25 18:11:48 +0100

    [add] controllare getApi

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <script>
 import axios from 'axios';
+import { store } from './data/store';
 import Main from './components/Main.vue'
 export default {
         components:{
           Main
+          
         },
         data(){
           return{
-
+            store
           }
         },
         methods:{
           getApi(){
             console.log('get api');
+            console.log(this.store);
+            axios.get(this.store.apiUrl, {
+              params:{
+                num: 12,
+                offset: 0,
+                language: 'it'
+              }
+            })
+            .then(result => {
+              
+              this.store.cardsList = result.data.data;
+              console.log(this.store.cardsList);
+            })
+            .catch(error => {
+              console.log(error);
+            })
           }
         },
         mounted(){
--- src/data/store.js
+++ src/data/store.js
@@ -2,5 +2,6 @@ import { reactive } from "vue"
 
 export const store = reactive({
     counter: 0,
-    apiUrl: 'https://rickandmortyapi.com/api/character'
+    apiUrl: 'https://rickandmortyapi.com/api/character',
+    cardsList: []
 })
