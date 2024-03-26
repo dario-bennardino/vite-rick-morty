1270e0650001b56b5884b4b59147440b5423be12
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-03-26 18:10:06 +0100

    [add] searchbar componente inserito

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -5,15 +5,18 @@
 </script>
 
 <template>
-    <label for="exampleDataList" class="form-label">Datalist example</label>
-    <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
-    <datalist id="datalistOptions">
-        <option value="San Francisco">
-        <option value="New York">
-        <option value="Seattle">
-        <option value="Los Angeles">
-        <option value="Chicago">
-    </datalist>
+        <label for="exampleDataList" class="form-label">Datalist example</label>
+        <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
+        <datalist id="datalistOptions">
+            <option value="Chicago"></option>
+            <option value="Denver"></option>
+            <option value="New York"></option>
+            <option value="San Francisco"></option>
+            <option value="Boston"></option>
+            
+        </datalist>
+
+
 </template>
 
 
