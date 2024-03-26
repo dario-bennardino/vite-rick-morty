2b678d0949b7c6cfd505abacfe324347338bbddb
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-03-26 14:39:28 +0100

    [add] esercizio parte I completo

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,9 @@ export default {
 
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
    @use './assets/scss/main.scss';  
+
+
 
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -19,14 +19,7 @@ import ProductCard from './partials/ProductCard.vue';
     <div class="text-center my-5 container">
 
         <div class="row row-cols-4">
-            <!-- <ProductCard 
-            v-for="card in this.store.cardsList" 
-            :key="card.results.id"
-            :name="card.results.name" 
-            :status="card.results.status"
-            :species="card.results.species"
-            :image="card.results.origin.url"
-            /> -->
+
             <ProductCard 
             v-for=" card in this.store.cardList"
             :key="card.id"
